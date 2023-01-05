7f493f26f0023a5ddb37f84425438fbd2c8f3386
deshondixon <deshondixon@gmail.com>
2023-01-04 16:31:01 -0800

    -

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -4,10 +4,10 @@ from data_structures.stack import Stack
 def multi_bracket_validation(string):
     stack = Stack()
     brackets = {"(": ")", "{": "}", "[": "]"}
-    for char in string:
-        if char in brackets:
-            stack.push(char)
-        elif char in brackets.values():
-            if stack.is_empty() or brackets[stack.pop()] != char:
+    for x in string:
+        if x in brackets:
+            stack.push(x)
+        elif x in brackets.values():
+            if stack.is_empty() or brackets[stack.pop()] != x:
                 return False
     return stack.is_empty()
